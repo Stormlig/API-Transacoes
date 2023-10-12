558a22584e5756540d9e6548dcb0f72ea51d8aa9
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-12 16:55:08 -0300

    feat: adicionado variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
 const { app } = require('./app');
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}`);
